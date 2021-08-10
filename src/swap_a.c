bdb40c8ce53e6d4a24746d8c128976493724aa64
OEM Configuration (temporary user) <mrnicolastito@icloud.com>
2021-08-10 20:35:27 +0100

    ra rb rr swap

--- src/swap_a.c
+++ src/swap_a.c
@@ -31,11 +31,38 @@ void	sa_sb(int *stack, int size, char c)
 
 void	ss(int *stack_a, int *stack_b, t_inf *s_l)
 {
-	if(s_l ->size_a < 2 || s_l->size_a < 2)
+	if(s_l->size_a < 2 || s_l->size_a < 2)
 		return ;
 	sa_sb(stack_a, s_l->size_a, 's');
 	sa_sb(stack_b, s_l->size_b, 's');
 	write(1, "ss\n", 3);
 }
 
+void	ra_rb(int *stack, int size, char c)
+{
+	int	temp;
+	int	i;
 
+	i = -1;
+	if (size < 2)
+		return ;
+	temp = stack[0];
+	while(++i < size)
+		stack[i] = stack[i + 1];
+	stack[size] = temp;
+	if (c = 'a')
+		write(1, "ra\n", 3);
+	if (c = 'b')
+		write(1, "rb\n", 3);
+	if (c = 'r')
+		return ;
+}
+
+void rr(int *stack_a, int *stack_b, t_inf *s_l)
+{
+	if (s_l->size_a < 2 || s_l->size_b < 2)
+		return ;
+	ra_rb (stack_a, s_l->size_a, 'r');
+	ra_rb (stack_b, s_l->size_b, 'r');
+	write(1, "rr\n", 3);
+}
